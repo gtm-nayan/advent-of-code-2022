c058068a4478d9797110d04ff7457b0a45f53bab
gtmnayan <gtmnayan@gmail.com>
2022-12-04 13:38:18 +0545

    day 4

---
+++ src/bin/04.rs
@@ -0,0 +1,75 @@
+#![feature(slice_split_at_unchecked)]
+use std::{iter::from_fn, str::from_utf8_unchecked};
+
+use memchr::memchr;
+
+fn split_and_parse(needle: u8, input: &[u8]) -> (u8, &[u8]) {
+    let loc = unsafe { memchr(needle, input).unwrap_unchecked() };
+
+    let (head, mut tail) = unsafe { input.split_at_unchecked(loc) };
+    tail = unsafe { tail.split_first().unwrap_unchecked().1 };
+    let head = unsafe { from_utf8_unchecked(head).parse::<u8>().unwrap_unchecked() };
+    (head, tail)
+}
+
+pub fn pairs(input: &str) -> impl Iterator<Item = (u8, u8, u8, u8)> + '_ {
+    let mut slice = input.as_bytes();
+
+    from_fn(move || {
+        if slice.is_empty() {
+            return None;
+        }
+
+        let (first_elf_start, tail) = split_and_parse(b'-', slice);
+        let (first_elf_end, tail) = split_and_parse(b',', tail);
+        let (second_elf_start, tail) = split_and_parse(b'-', tail);
+        let (second_elf_end, tail) = split_and_parse(b'\n', tail);
+        slice = tail;
+
+        Some((
+            first_elf_start,
+            first_elf_end,
+            second_elf_start,
+            second_elf_end,
+        ))
+    })
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(
+        pairs(input)
+            .filter(|(a, b, c, d)| (a <= c) && (d <= b) || (c <= a) && (b <= d))
+            .count() as u32,
+    )
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(
+        pairs(input)
+            .filter(|(a, b, c, d)| a.max(c) <= b.min(d))
+            .count() as u32,
+    )
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 4);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 4);
+        assert_eq!(part_one(&input), Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 4);
+        assert_eq!(part_two(&input), None);
+    }
+}
