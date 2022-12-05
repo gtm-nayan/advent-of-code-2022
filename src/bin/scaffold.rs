0bbf7dba290424300341d079e1d4bf6aacc0bb8b
gtmnayan <gtmnayan@gmail.com>
2022-12-05 19:16:47 +0545

    move out template into its own file

--- src/bin/scaffold.rs
+++ src/bin/scaffold.rs
@@ -8,37 +8,7 @@ use std::{
     process,
 };
 
-const MODULE_TEMPLATE: &str = r###"pub fn part_one(input: &str) -> Option<u32> {
-    None
-}
-
-pub fn part_two(input: &str) -> Option<u32> {
-    None
-}
-
-fn main() {
-    let input = &advent_of_code::read_file("inputs", DAY);
-    advent_of_code::solve!(1, part_one, input);
-    advent_of_code::solve!(2, part_two, input);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let input = advent_of_code::read_file("examples", DAY);
-        assert_eq!(part_one(&input), None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let input = advent_of_code::read_file("examples", DAY);
-        assert_eq!(part_two(&input), None);
-    }
-}
-"###;
+const MODULE_TEMPLATE: &str = include_str!(".template.rs");
 
 fn parse_args() -> Result<u8, pico_args::Error> {
     let mut args = pico_args::Arguments::from_env();
