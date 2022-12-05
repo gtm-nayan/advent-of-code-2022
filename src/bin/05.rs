27eb4bd3d50bacf52a875b3243d5990bc2365dd8
gtmnayan <gtmnayan@gmail.com>
2022-12-05 19:06:18 +0545

    gahhh, I need to stop doing this, (-13 microsecs)

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -2,7 +2,7 @@
 #![feature(assert_matches)]
 #![feature(get_many_mut)]
 
-use std::collections::VecDeque;
+use std::iter::from_fn;
 
 #[derive(Debug)]
 struct Stacks {
@@ -35,11 +35,31 @@ struct Move {
     count: usize,
 }
 
-impl Move {
-    pub fn from_line(line: &str) -> Self {
-        let mut chars = line.chars();
+fn parse_input(input: &str) -> (Stacks, impl Iterator<Item = Move> + '_) {
+    let mut chars = input.chars();
 
-        let mut get_num = move || {
+    let num = (memchr::memchr(b'\n', input.as_bytes()).unwrap() + 1) / 4;
+
+    let mut stacks = vec![Vec::new(); num];
+
+    chars
+        .by_ref()
+        .enumerate()
+        .skip(1)
+        .step_by(2)
+        .take_while(|&(_, ch)| ch != 'm')
+        .filter(|(_, ch)| ch.is_ascii_uppercase())
+        .for_each(|(pos, ch)| {
+            unsafe { stacks.get_unchecked_mut((pos / 4) % num) }.push(ch);
+        });
+
+    for stack in stacks.iter_mut() {
+        stack.reverse();
+    }
+
+    let moves = from_fn(move || {
+        chars.next()?;
+        let mut get_num = || {
             chars
                 .by_ref()
                 .skip_while(|ch| !ch.is_ascii_digit())
@@ -51,41 +71,10 @@ impl Move {
         let from = get_num() - 1;
         let to = get_num() - 1;
 
-        Move { from, to, count }
-    }
-}
+        Some(Move { from, to, count })
+    });
 
-fn parse_crate_line(input: &str) -> impl Iterator<Item = (usize, char)> + '_ {
-    input
-        .chars()
-        .enumerate()
-        .filter(|(_, ch)| ch.is_ascii_alphabetic())
-        .map(|(pos, ch)| (pos / 4, ch))
-}
-
-fn parse_input(input: &str) -> (Stacks, impl Iterator<Item = Move> + '_) {
-    let first = input.lines().next().unwrap();
-    let mut crates = vec![VecDeque::<char>::new(); (first.len() + 1) / 4];
-
-    let mut lines = input.lines();
-
-    lines
-        .by_ref()
-        .take_while(|l| !l.is_empty())
-        .flat_map(parse_crate_line)
-        .for_each(|(stack, cr)| {
-            let stack = crates.get_mut(stack).unwrap();
-
-            stack.push_front(cr)
-        });
-
-    let stacks = Stacks {
-        stacks: crates.into_iter().map(Vec::from).collect(),
-    };
-
-    let moves = lines.map(Move::from_line);
-
-    (stacks, moves)
+    (Stacks { stacks }, moves)
 }
 
 pub fn part_one(input: &str) -> Option<String> {
@@ -116,30 +105,8 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use std::assert_matches::assert_matches;
 
     use super::*;
-
-    #[test]
-    fn test_parse_move() {
-        let line = "move 1 from 2 to 1";
-
-        assert_matches!(
-            Move::from_line(line),
-            Move {
-                from: 1,
-                to: 0,
-                count: 1
-            }
-        )
-    }
-
-    #[test]
-    fn test_parse_crate_line() {
-        let line = "[A]     [B]";
-
-        assert!(parse_crate_line(line).eq([(0_usize, 'A'), (2_usize, 'B')].into_iter()))
-    }
 
     #[test]
     fn test_part_one() {
